c7d3293f3d7858c93fcaa3501fbaef03746139a2
Gaston Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-06-27 19:54:14 -0300

    Footer Working

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import '../styles/Home-Page.css'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer"></div>
+        </>
+    )
+}
+
+export default Footer
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,11 +1,23 @@
 import Header from "./Header";
 import '../styles/Home-Page.css'
+import Footer from "./Footer";
 
 function Inicio() {
     return (
         <>
-            <Header/>
-            <h1>Pagina de Inicio</h1>
+            <Header />
+            <div className="first">
+                <p className="flyers">Inserte flyer nro. 1 Aqui</p>
+            </div>
+            <div className="flyers-container">
+                <div className="flyers another-flyers">
+                    <p className="flyers">Inserte flyer nro. 2 Aqui</p>
+                </div>
+                <div className="flyers another-flyers">
+                    <p className="flyers">Inserte flyer nro. 3 Aqui</p>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
